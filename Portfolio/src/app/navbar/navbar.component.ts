5be8f751f53191bee9d1ee404aebd1be84ee07c6
DavidV0 <davidvelickovic@hotmail.com>
2024-03-15 20:07:35 +0100

    navbar

--- Portfolio/src/app/navbar/navbar.component.ts
+++ Portfolio/src/app/navbar/navbar.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-navbar' ,
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  about: boolean = false;
+  skills: boolean = false;
+  portfolio: boolean = false;
+    clickEvent(id: number) {
 
+    if(id == 0){
+      this.about = !this.about
+      this.skills = false;
+      this.portfolio = false
+    }else if(id == 1){
+      this.skills = !this.skills
+      this.about = false
+      this.portfolio = false
+    }else if(id == 2){
+      this.portfolio = !this.portfolio
+      this.about = false;
+      this.skills = false
+    }
+  }
 }
