cc34b91c4589e06c4fcf396143fce07ccbd1d4a7
DavidV0 <davidvelickovic@hotmail.com>
2024-03-18 12:30:12 +0100

    above the fold section complete

--- Portfolio/src/app/shared/navbar/navbar.component.ts
+++ Portfolio/src/app/shared/navbar/navbar.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild} from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar' ,
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+
+  @Input() language = 'DE';
+  @ViewChild('checkbox', { static: false }) checkbox!: ElementRef;
   about: boolean = false;
   skills: boolean = false;
   portfolio: boolean = false;
@@ -26,6 +30,46 @@ export class NavbarComponent {
       this.portfolio = !this.portfolio
       this.about = false;
       this.skills = false
+    }
+  }
+
+
+  showOverlay = false;
+
+  goTo(destination: string) {
+    let target = document.querySelector(destination);
+    if (target!.id === 'portfolio') {
+      const yoffset = -100;
+      window.scroll({
+        top: target!.getBoundingClientRect().top + window.scrollY + yoffset,
+        behavior: 'smooth',
+      });
+      this.checkbox.nativeElement.checked = false;
+      this.showOverlay = false;
+      document.body.style.overflow = 'auto';
+    } else if (target!.id === 'skills') {
+      const yoffset = -500;
+      window.scroll({
+        top: target!.getBoundingClientRect().top + window.scrollY + yoffset,
+        behavior: 'smooth',
+      });
+      this.checkbox.nativeElement.checked = false;
+      this.showOverlay = false;
+      document.body.style.overflow = 'auto';
+    } else if (target!) {
+      target!.scrollIntoView();
+      this.checkbox.nativeElement.checked = false;
+      this.showOverlay = false;
+      document.body.style.overflow = 'auto';
+    }
+  }
+
+  openMobileMenu() {
+    this.showOverlay = !this.showOverlay;
+    if (this.showOverlay) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
     }
   }
 }
