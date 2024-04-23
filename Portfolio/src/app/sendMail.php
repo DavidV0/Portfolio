1295effec154bbe4c74c717221523fae2e3dd6f8
DavidV0 <davidvelickovic@hotmail.com>
2024-04-23 17:28:34 +0200

    fixes

--- Portfolio/src/app/sendMail.php
+++ Portfolio/src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'davidvelickovic@hotmail.com';  
+            $recipient = 'contact@david-velickovic.at';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
