9ac6b8867cb8922026781b001ac8a3f4c116e3bd
DavidV0 <davidvelickovic@hotmail.com>
2024-07-21 11:03:31 +0200

    small bug fix

--- Portfolio/src/app/sendMail.php
+++ Portfolio/src/app/sendMail.php
@@ -1,37 +1,49 @@
 <?php
 
 switch ($_SERVER['REQUEST_METHOD']) {
-    case ("OPTIONS"): //Allow preflighting to take place.
+    case ("OPTIONS"): // Allow preflighting to take place.
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: POST");
         header("Access-Control-Allow-Headers: content-type");
         exit;
-        case("POST"): //Send the email;
-            header("Access-Control-Allow-Origin: *");
-            // Payload is not send to $_POST Variable,
-            // is send to php:input as a text
-            $json = file_get_contents('php://input');
-            //parse the Payload from text format to Object
-            $params = json_decode($json);
-    
-            $email = $params->email;
-            $name = $params->name;
-            $message = $params->message;
-    
-            $recipient = 'contact@david-velickovic.at';  
-            $subject = "Contact From <$email>";
-            $message = "From:" . $name . "<br>" . $message ;
-    
-            $headers   = array();
-            $headers[] = 'MIME-Version: 1.0';
-            $headers[] = 'Content-type: text/html; charset=utf-8';
 
-            // Additional headers
-            $headers[] = "From: noreply@mywebsite.com";
+    case("POST"): // Send the email.
+        header("Access-Control-Allow-Origin: *");
 
-            mail($recipient, $subject, $message, implode("\r\n", $headers));
-            break;
-        default: //Reject any non POST or OPTIONS requests.
-            header("Allow: POST", true, 405);
-            exit;
-    } 
+        // Payload is not sent to $_POST Variable, it's sent to php:input as text
+        $json = file_get_contents('php://input');
+
+        // Parse the Payload from text format to Object
+        $params = json_decode($json);
+
+        $email = $params->email;
+        $name = $params->name;
+        $message = $params->message;
+
+        $recipient = 'contact@david-velickovic.at';  
+        $subject = "Contact From <$email>";
+        
+        // Construct the message properly
+        $body = "From: $name <$email><br><br>$message";
+
+        $headers   = array();
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=utf-8';
+
+        // Additional headers
+        $headers[] = "From: noreply@david-velickovic.at";
+
+        // Log the email details for debugging
+        error_log("Recipient: $recipient");
+        error_log("Subject: $subject");
+        error_log("Body: $body");
+        error_log("Headers: " . implode("\r\n", $headers));
+
+        // Send the email
+        mail($recipient, $subject, $body, implode("\r\n", $headers));
+        break;
+
+    default: // Reject any non-POST or OPTIONS requests.
+        header("Allow: POST", true, 405);
+        exit;
+}
